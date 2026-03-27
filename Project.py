715cd6204ec8d6d2e856db2bbb4f331c2450d7df
Ishan817 <ishan@example.com>
2026-03-28 03:06:54 +0530

    Removed unnecessary files

--- Project.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import mediapipe as mp
-from ultralytics import YOLO
-from deep_sort_realtime.deepsort_tracker import DeepSort
-import tensorflow as tf
-
-print("All installed successfully and imported without errors!")
